d541c909c06a73e9a05b57cedf9eba8207d15c4a
menady0 <menady20@gmail.com>
2024-10-14 14:31:53 +0300

    HTML & CSS for Menu page

--- src/app/Components/menu-page/menu-page.component.ts
+++ src/app/Components/menu-page/menu-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-page.component.html',
   styleUrl: './menu-page.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { MenuPageComponent } from './Components/menu-page/menu-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'menu', component: MenuPageComponent}
+];
